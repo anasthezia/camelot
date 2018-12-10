1cc792db6c67fac84336171a9b046c574321fce3
anasthezia <anasthezia161@gmail.com>
2018-12-11 00:44:41 +0300

    fix styles

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ window.countTo = require("jquery-countto");
 const $document=$(document);
 
 $document.ready(function(){
-    $( ".counter" ).each(function( index ) {
+    $( ".js-counter" ).each(function( index ) {
         console.log( index + ": " + $( this ).text() );
     });
 });
